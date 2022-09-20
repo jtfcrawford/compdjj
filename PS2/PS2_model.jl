27fb249c024cca46aca4c56b4d0925bdbc9cdb7d
Jon Kroah <jgkroah@gmail.com>
2022-09-20 17:35:55 -0500

    Fixed VFI, added T-star operator
    - Fixed VFI (we were using V(a,s') in the continuation value, not V(a', s'))
    - Added T-star operator and a T-star iteration function. However it's yielding some \mu's which don't sum to 1 -- I made the program return an error and stop if this happens, but not sure if this is necessary

--- PS2/PS2_model.jl
+++ PS2/PS2_model.jl
@@ -10,15 +10,20 @@ using Parameters
 
 # Input - exogenous selections, also including grid parameters
 @with_kw struct Input
-    beta::Float64 = 0.9932 # discount factor
-    alpha::Float64 = 1.5 # coefficient of relative risk aversion
-    S::Array{Float64,1} = [1.0,0.5] # set of possible earnings, e=1,u=0.5
-    Π::Array{Float64,2} = [0.97 0.5; 0.03 0.5] # same thing: transition probabilities matrix but to avoid issue
-    A::Array{Float64,1} = [-2.0,5.0] # space of asset holdings
+    β::Float64 = 0.9932 # discount factor
+    α::Float64 = 1.5 # coefficient of relative risk aversion
+    
+    S::Array{Float64,1} = [1.0, 0.5] # set of possible earnings, e=1,u=0.5
+    Π::Array{Float64,2} = [0.97 0.03; 0.5 0.5] # same thing: transition probabilities matrix but to avoid issue
+    S_length::Int64 = length(S)
+
+    A::Array{Float64,1} = [-2.0, 5.0] # space of asset holdings
     a_length::Int64 = 1000 # asset grid length, count
     a_grid::Array{Float64,1} = range(start=A[1],length=a_length,stop=A[2]) # asset grid
     q::Float64 = 0.5 # price of discount bonds
-    q_min::Float64 = 0
+    
+    q_initial::Float64 = 0.5 # initial guess for price of discount bonds
+    q_min::Float64 = 0 # range of possible bond prices
     q_max::Float64 = 1
 end
 
@@ -26,7 +31,30 @@ end
 mutable struct Output
     valfunc::Array{Float64,2} # value function
     polfunc::Array{Float64,2} # policy function
-    mu::Array{Float64,2} # bond holdings / wealth distribution
+    μ::Array{Float64,2} # bond holdings / wealth distribution
+    q::Float64 # equilibrium price of discount bond
+end
+
+## Initialize structures
+function Initialize()
+    # initialize struct with model primitives
+    input = Input()
+
+    # initialize struct with results
+    valfunc = zeros(input.a_length, input.S_length)
+    polfunc = zeros(input.a_length, input.S_length)
+
+    # initial guess for stationary wealth distribution
+    # try a uniform distribution (PS2 suggests something else -- maybe replace later)
+    μ = fill((input.a_length * input.S_length)^(-1), input.a_length, input.S_length)
+
+    # initial guess for market-clearing price
+    q = input.q_initial
+
+    # initialize struct with equilibrum objects to solve for
+    output = Output(valfunc, polfunc, μ, q)
+
+    return input, output
 end
 
 ## 2 - Value Function Iteration ##
@@ -34,41 +62,42 @@ end
 function Bellman(input::Input,output::Output)
 # This function is to be called each value function iteration.
 # It returns the next guess of the value function.
-    @unpack valfunc = output # get last value function guess, as given
-    @unpack beta, alpha, S, Π, A, a_length, a_grid = input # get parameters
-    v_next = zeros(a_length,2) # Initializing the next guess as zeroes, 2 for number of emp. states
+    @unpack valfunc, q = output # get last guesses for value function and bond price
+    @unpack β, α, S, S_length, Π, A, a_length, a_grid = input # get parameters
+    
+    v_next = zeros(a_length, 2) # Initializing the next guess as zeroes, 2 for number of emp. states
+    
     # for loop over emp states
-    for e_index = 1:2 #iterating over values of emp. states
+    for e_index = 1:2, a_index = 1:a_length # loop over (a,s)
+        
         e = S[e_index] # which emp. state
-        candidate_max = -100000 # initial candidate
-        # for loop over asset grid
-        for a_index = 1:a_length  
-            a = a_grid[a_index] 
-            # for loop over choices of next a
-            for ap_index = 1:a_length
-                ap = a_grid[ap_index]
-                c = e + a - ap*q # consumption
-                if c>0 
-                    val = ((c^(1-alpha)-1)/(1-alpha)) + beta*(Π[e_index, 1]*valfunc[a_index, 1] + Π[e_index, 2]*valfunc[a_index, 2])
-                    if val > candidate_max
-                        candidate_max = val
-                        output.polfunc[a_index,e_index] = ap # Updating decision rule
-                    end
+        a = a_grid[a_index]
+        
+        # for loop over choices of next a'
+        candidate_max = -Inf # initial candidate
+        for ap_index = 1:a_length
+            ap = a_grid[ap_index]
+            c = e + a - ap*q # consumption
+            if c > 0
+                val = ((c^(1 - α) - 1)/(1 - α)) + β*(Π[e_index, 1] * valfunc[ap_index, 1] + Π[e_index, 2] * valfunc[ap_index, 2])
+                if val > candidate_max
+                    candidate_max = val
+                    output.polfunc[a_index,e_index] = ap # Updating decision rule
                 end
             end
-            v_next[a_index,e_index] = candidate_max
         end
-    end
+        v_next[a_index,e_index] = candidate_max
+    end # end loop over (a,s)
     return v_next
 end
 
-function Value_Iteration(input::Input,output::Output,tol::Float64 = 1e-5)
+function Value_Iteration(input::Input,output::Output; tol::Float64 = 1e-5)
 # This function re-runs the Bellman function until sufficient value function convergence
     error = 1000.0 # Some large number so the loop doesn't immediately stop
     counter = 0
     while error > tol
         v_next = Bellman(input,output)  
-        error = abs(maximum(v_next-output.valfunc)) / abs(maximum(v_next))
+        error = maximum(abs.(v_next .- output.valfunc))
         #print(string(maximum(output.valfunc)) * '\n')
         output.valfunc = v_next
         counter = counter + 1
@@ -84,13 +113,47 @@ end
 # "What is the probability that an employed person (in state e) with assets worth one
 # year's income (in state a=1) is in the third wealth quintile next year?" for example.
 
-function new_Π(input::Input,output::Output)
+function T_star(input::Input,output::Output)
+    @unpack S, Π, S_length, A, a_length, a_grid = input
+    @unpack polfunc, μ = output
 
+    # initial guess for next iteration of μ
+    μ_next = zeros(a_length, S_length)
+    
+    # μ_next[i_next, j_next] = Pr(a' = a_grid[i_next], s' = S[j_next])
+    # Using a law of motion, this is:
+    #   whether a HH will have a'=a[i_next] tomorrow, given they today have a=a_grid[i_today] and are in state S[j_today], times
+    #   the probability that a HH is today in state S[j_today] and goes to S[j_next] tomorrow, times
+    #   the measure of HHs who today have a=a_grid[i_today] and are in state S[j_today]
+    # ... summed over all assets a_grid[i_today] and all states S[j_today]
+    for i_next=1:a_length, j_next=1:S_length
+        for i_today=1:a_length, j_today=1:S_length
+            μ_next[i_next,j_next] += (polfunc[i_today, j_today] == a_grid[i_next]) * Π[j_today, j_next] * μ[i_today, j_today]
+        end
+    end
+    
+    # check that μ_next approximately sums to 1 (code breaks if not)
+    sum_check = sum(μ_next)
+    @assert abs(sum(μ_next) - 1) < 0.01 "Error: sum of μ_next is $sum_check; not within 0.01 of 1."
+
+    # update μ
+    return μ_next
+end
+
+function Wealth_Dist_Iteration(input::Input, output::Output; tol::Float64=1e-5, err::Float64 = 100.0)
+    counter = 0
+    while err > tol
+        μ_next = T_star(input, output) # apply T* operator to current guess for μ*
+        err = maximum(abs.((μ_next .- output.μ) ./ output.μ)) # update error
+        output.μ = μ_next
+        counter += 1
+    end
+    println("Wealth distribution converged in $counter iterations")
 end
 
 # Then we need another function that basically is the same as the value iteration function
 # except we want the wealth distribution to converge
-
+#=
 function dist_iteration(input::Input,output::Output)
 
 end
@@ -98,8 +161,9 @@ end
 # Once we have the above set up, we can use the below price updating function and continually
 # re-run value iteration and wealth distribution iteration until we find the "right" price
 
+
 function adjust_price(input::Input,output::Output,tol::Float64 = 1e-5)
-   excess_supply = sum(output.mu .* [a_grid a_grid])
+   excess_supply = sum(output.μ .* [a_grid a_grid])
    if abs(excess_supply) < tol
 
    # I think I was close on how to do this bisection but the below isn't quite right
@@ -110,3 +174,4 @@ function adjust_price(input::Input,output::Output,tol::Float64 = 1e-5)
    # output.q = (output.q + q_min) / 2
    end
 end
+=#
--- PS2/PS2_script.jl
+++ PS2/PS2_script.jl
@@ -7,29 +7,28 @@
 println("Starting...")
 
 # You may need to manually set your file path in the Julia terminal using pwd("C:\\Example\\Filepath")
+# Or you can change this line of code:
+cd("C:\\Users\\jgkro\\Documents\\GitHub\\compdjj\\PS2")
 
 # Bring in model and other functions
 include("PS2_model.jl")
 
-# Initialize
-input = Input()
-@unpack a_length, a_grid = Input()
-valfunc = zeros(a_length,2)
-polfunc = zeros(a_length,2)
-mu = zeros(a_length,2)
-q = 0.5
-q_min = 0
-q_max = 1
-output = Output(valfunc,polfunc,mu)
+# Initialize input (primitives) and output (solutions) structures
+input, output = Initialize()
 
-# Solve
+# Find value function & policy function
 Value_Iteration(input,output)
+
+# Find stationary wealth distribution
+Wealth_Dist_Iteration(input, output)
 
 # Plotting
 using Plots
-plot(a_grid, output.valfunc,labels=["Employed" "Unemployed"])
-savefig("PS2\\valfunc.png")
-plot(a_grid, output.polfunc,labels=["Employed" "Unemployed"])
-savefig("PS2\\polfunc.png")
+plot(input.a_grid, output.valfunc,labels=["Employed" "Unemployed"])
+# savefig("PS2\\valfunc.png")
+savefig("valfunc.png")
+plot(input.a_grid, output.polfunc,labels=["Employed" "Unemployed"])
+# savefig("PS2\\polfunc.png")
+savefig("polfunc.png")
 
 println("Done!")
